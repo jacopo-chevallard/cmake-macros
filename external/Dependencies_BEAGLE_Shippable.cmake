382f4f224d830c58367afdbab4de106a59a21995
jacopo-chevallard <jacopo.chevallard@gmail.com>
2016-10-07 17:03:21 +0200

    Added dependency on FLAP

--- external/Dependencies_BEAGLE_Shippable.cmake
+++ external/Dependencies_BEAGLE_Shippable.cmake
@@ -3,6 +3,7 @@ include(Dependencies_mcfor_Shippable)
 set (Dependencies_BEAGLE
   multivar_splines%multivariate_splines.mod
   bc03_utilities
+  flap%flap.mod
   mcfor%prosit.mod
   ${Dependencies_mcfor}
 )
@@ -10,6 +11,7 @@ set (Dependencies_BEAGLE
 set (Dependencies_names_BEAGLE
   multivar_splines
   bc03_utilities
+  flap
   mcfor
   ${Dependencies_names_mcfor}
 )

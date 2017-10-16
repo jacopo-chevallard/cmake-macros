436a6c0b2e9780e7b477bd1045cd728b68cbddda
jacopo-chevallard <jacopo.chevallard@gmail.com>
2017-10-16 15:44:09 +0200

    Added function to add nested libraries

--- addNestedDependencies.cmake
+++ addNestedDependencies.cmake
@@ -5,42 +5,60 @@ function (addNestedIncludeDirectories dependency_name include_directories_list)
   string(REPLACE "%" ";" dependency ${dependency_name})
   list(GET dependency 0 dependency_lib_name)
 
+  if (NESTED_DEPENDENCIES)
 
-  #list(FIND target_link_libraries_list "${dependency_lib_name}" HasLibrary)
-  #IF(HasLibrary GREATER -1)
-  #  return()
-  #ENDIF()
+    set(include_directories_local "${include_directories_list}")
 
-  # MESSAGE(STATUS "dependency_lib_name: ${dependency_lib_name}")
+    foreach (sub_dependency ${Dependencies_${dependency_lib_name}})
+
+      string(REPLACE "%" ";" sub_dependency_name ${sub_dependency})
+      list(GET sub_dependency_name 0 sub_dependency_lib_name)
+
+      # Library name
+      addNestedIncludeDirectories(${sub_dependency} "${include_directories_local}")
+      getDependencyPath(${sub_dependency})
+
+      string(TOUPPER ${sub_dependency_lib_name} tmp)
+      set(include_directories_local ${include_directories_local} "${${tmp}_INCLUDE_DIR}")
+
+    endforeach(sub_dependency)
+
+    set(include_directories_list ${include_directories_local} PARENT_SCOPE)
+
+  ENDif (NESTED_DEPENDENCIES)
+
+endfunction()
+
+
+# -------------------------------------------------------------------------------
+# 
+# -------------------------------------------------------------------------------
+function (addNestedLibraries dependency_name target_link_libraries_list)
+
+  string(REPLACE "%" ";" dependency ${dependency_name})
+  list(GET dependency 0 dependency_lib_name)
 
   include(Dependencies_${dependency_lib_name}${SHIPPABLE_SUFFIX} OPTIONAL RESULT_VARIABLE NESTED_DEPENDENCIES)
 
   if (NESTED_DEPENDENCIES)
 
-    #set(target_link_libraries_local "${target_link_libraries_list}")
-    set(include_directories_local "${include_directories_list}")
+    set(target_link_libraries_local "${target_link_libraries_list}")
 
     foreach (sub_dependency ${Dependencies_${dependency_lib_name}})
 
-      #MESSAGE(STATUS "target_link_libraries_list function: ${target_link_libraries_list}")
       string(REPLACE "%" ";" sub_dependency_name ${sub_dependency})
       list(GET sub_dependency_name 0 sub_dependency_lib_name)
-      #MESSAGE(STATUS "dependency function: ${sub_dependency}")
 
       # Library name
-      addNestedIncludeDirectories(${sub_dependency} "${include_directories_local}")
+      addNestedLibraries(${sub_dependency} "${target_link_libraries_local}")
       getDependencyPath(${sub_dependency})
-      #set (LIB_INCLUDEDIR ${CMAKE_INSTALL_PREFIX}/include/${dependency})
-      #set (LIB_NAME "${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_LIBRARY_PREFIX}${dependency}${CMAKE_LIBRARY_SUFFIX}")
 
       string(TOUPPER ${sub_dependency_lib_name} tmp)
-      set(include_directories_local ${include_directories_local} "${${tmp}_INCLUDE_DIR}")
-      #set(target_link_libraries_local ${target_link_libraries_local} "${${tmp}_LIBRARIES}")
+      set(target_link_libraries_local ${target_link_libraries_local} "${${tmp}_LIBRARIES}")
 
     endforeach(sub_dependency)
 
-    set(include_directories_list ${include_directories_local} PARENT_SCOPE)
-    #set(target_link_libraries_list ${target_link_libraries_local} PARENT_SCOPE)
+    set(target_link_libraries_list ${target_link_libraries_local} PARENT_SCOPE)
 
   ENDif (NESTED_DEPENDENCIES)
 

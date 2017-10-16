d72bb5ce6468cfd60d8c582ecc64f457e0376969
jacopo-chevallard <jacopo.chevallard@gmail.com>
2017-10-16 18:48:36 +0200

    Now functions are correctly (recursively) creating a list of include directories and link libraries

--- addNestedDependencies.cmake
+++ addNestedDependencies.cmake
@@ -1,45 +1,92 @@
 include(getDependencyPath)
 
-function (addNestedDependencies dependency_name target_link_libraries_list include_directories_list)
+# -------------------------------------------------------------------------------
+# 
+# -------------------------------------------------------------------------------
+function (addNestedIncludeDirectories dependency_name include_directories_list)
+
+  set(recursion FALSE)
+  set (extra_macro_args ${ARGN})
+
+  # Did we get any optional args?
+  list(LENGTH extra_macro_args num_extra_args)
+  if (${num_extra_args} GREATER 0)
+    set(recursion TRUE)
+  endif ()
+
+  if (NOT recursion)
+    set(include_directories_local "${include_directories_list}" CACHE INTERNAL "" )
+  endif (NOT recursion)
 
   string(REPLACE "%" ";" dependency ${dependency_name})
   list(GET dependency 0 dependency_lib_name)
-
-
-  #list(FIND target_link_libraries_list "${dependency_lib_name}" HasLibrary)
-  #IF(HasLibrary GREATER -1)
-  #  return()
-  #ENDIF()
-
-  # MESSAGE(STATUS "dependency_lib_name: ${dependency_lib_name}")
 
   include(Dependencies_${dependency_lib_name}${SHIPPABLE_SUFFIX} OPTIONAL RESULT_VARIABLE NESTED_DEPENDENCIES)
 
   if (NESTED_DEPENDENCIES)
 
-    set(target_link_libraries_local "${target_link_libraries_list}")
     set(include_directories_local "${include_directories_list}")
 
     foreach (sub_dependency ${Dependencies_${dependency_lib_name}})
 
-      #MESSAGE(STATUS "target_link_libraries_list function: ${target_link_libraries_list}")
       string(REPLACE "%" ";" sub_dependency_name ${sub_dependency})
       list(GET sub_dependency_name 0 sub_dependency_lib_name)
-      #MESSAGE(STATUS "dependency function: ${sub_dependency}")
 
       # Library name
-      addNestedDependencies(${sub_dependency} "${target_link_libraries_local}" "${include_directories_local}")
+      addNestedIncludeDirectories(${sub_dependency} "${include_directories_local}" TRUE)
       getDependencyPath(${sub_dependency})
-      #set (LIB_INCLUDEDIR ${CMAKE_INSTALL_PREFIX}/include/${dependency})
-      #set (LIB_NAME "${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_LIBRARY_PREFIX}${dependency}${CMAKE_LIBRARY_SUFFIX}")
 
       string(TOUPPER ${sub_dependency_lib_name} tmp)
-      set(include_directories_local ${include_directories_local} "${${tmp}_INCLUDE_DIR}")
-      set(target_link_libraries_local ${target_link_libraries_local} "${${tmp}_LIBRARIES}")
+      set(include_directories_local ${include_directories_local} "${${tmp}_INCLUDE_DIR}" CACHE INTERNAL "")
 
     endforeach(sub_dependency)
 
     set(include_directories_list ${include_directories_local} PARENT_SCOPE)
+
+  ENDif (NESTED_DEPENDENCIES)
+
+endfunction()
+
+
+# -------------------------------------------------------------------------------
+# 
+# -------------------------------------------------------------------------------
+function (addNestedLibraries dependency_name target_link_libraries_list)
+
+  set(recursion FALSE)
+  set (extra_macro_args ${ARGN})
+
+  # Did we get any optional args?
+  list(LENGTH extra_macro_args num_extra_args)
+  if (${num_extra_args} GREATER 0)
+    set(recursion TRUE)
+  endif ()
+
+  if (NOT recursion)
+    set(target_link_libraries_local "${target_link_libraries_list}" CACHE INTERNAL "" )
+  endif (NOT recursion)
+
+  string(REPLACE "%" ";" dependency ${dependency_name})
+  list(GET dependency 0 dependency_lib_name)
+
+  include(Dependencies_${dependency_lib_name}${SHIPPABLE_SUFFIX} OPTIONAL RESULT_VARIABLE NESTED_DEPENDENCIES)
+
+  if (NESTED_DEPENDENCIES)
+
+    foreach (sub_dependency ${Dependencies_${dependency_lib_name}})
+
+      string(REPLACE "%" ";" sub_dependency_name ${sub_dependency})
+      list(GET sub_dependency_name 0 sub_dependency_lib_name)
+
+      # Library name
+      addNestedLibraries(${sub_dependency} "${target_link_libraries_local}" TRUE)
+      getDependencyPath(${sub_dependency})
+
+      string(TOUPPER ${sub_dependency_lib_name} tmp)
+      set(target_link_libraries_local ${target_link_libraries_local} "${${tmp}_LIBRARIES}" CACHE INTERNAL "" )
+
+    endforeach(sub_dependency)
+
     set(target_link_libraries_list ${target_link_libraries_local} PARENT_SCOPE)
 
   ENDif (NESTED_DEPENDENCIES)

9d03d1e9c6e7c50464ff9183db784e33e697bf48
jacopo-chevallard <jacopo.chevallard@gmail.com>
2017-10-16 17:07:08 +0200

    Debugging...

--- addNestedDependencies.cmake
+++ addNestedDependencies.cmake
@@ -1,6 +1,22 @@
 include(getDependencyPath)
 
+# -------------------------------------------------------------------------------
+# 
+# -------------------------------------------------------------------------------
 function (addNestedIncludeDirectories dependency_name include_directories_list)
+
+  set(recursion FALSE)
+  set (extra_macro_args ${ARGN})
+
+  # Did we get any optional args?
+  list(LENGTH extra_macro_args num_extra_args)
+  if (${num_extra_args} GREATER 0)
+    set(recursion TRUE)
+  endif ()
+
+  if (NOT recursion)
+    set(include_directories_local "${include_directories_list}" CACHE INTERNAL "" )
+  endif (NOT recursion)
 
   string(REPLACE "%" ";" dependency ${dependency_name})
   list(GET dependency 0 dependency_lib_name)
@@ -17,11 +33,11 @@ function (addNestedIncludeDirectories dependency_name include_directories_list)
       list(GET sub_dependency_name 0 sub_dependency_lib_name)
 
       # Library name
-      addNestedIncludeDirectories(${sub_dependency} "${include_directories_local}")
+      addNestedIncludeDirectories(${sub_dependency} "${include_directories_local}" TRUE)
       getDependencyPath(${sub_dependency})
 
       string(TOUPPER ${sub_dependency_lib_name} tmp)
-      set(include_directories_local ${include_directories_local} "${${tmp}_INCLUDE_DIR}")
+      set(include_directories_local ${include_directories_local} "${${tmp}_INCLUDE_DIR}" CACHE INTERNAL "")
 
     endforeach(sub_dependency)
 
@@ -37,14 +53,28 @@ endfunction()
 # -------------------------------------------------------------------------------
 function (addNestedLibraries dependency_name target_link_libraries_list)
 
+  set(recursion FALSE)
+  set (extra_macro_args ${ARGN})
+
+  # Did we get any optional args?
+  list(LENGTH extra_macro_args num_extra_args)
+  if (${num_extra_args} GREATER 0)
+    set(recursion TRUE)
+  endif ()
+
+  if (NOT recursion)
+    set(target_link_libraries_local "${target_link_libraries_list}" CACHE INTERNAL "" )
+  endif (NOT recursion)
+
+  MESSAGE(STATUS "dependency_name----------> " ${dependency_name})
+
   string(REPLACE "%" ";" dependency ${dependency_name})
   list(GET dependency 0 dependency_lib_name)
 
+  MESSAGE(STATUS "dependency_lib_name----------> " ${dependency_lib_name})
   include(Dependencies_${dependency_lib_name}${SHIPPABLE_SUFFIX} OPTIONAL RESULT_VARIABLE NESTED_DEPENDENCIES)
 
   if (NESTED_DEPENDENCIES)
-
-    set(target_link_libraries_local "${target_link_libraries_list}")
 
     foreach (sub_dependency ${Dependencies_${dependency_lib_name}})
 
@@ -52,11 +82,11 @@ function (addNestedLibraries dependency_name target_link_libraries_list)
       list(GET sub_dependency_name 0 sub_dependency_lib_name)
 
       # Library name
-      addNestedLibraries(${sub_dependency} "${target_link_libraries_local}")
+      addNestedLibraries(${sub_dependency} "${target_link_libraries_local}" TRUE)
       getDependencyPath(${sub_dependency})
 
       string(TOUPPER ${sub_dependency_lib_name} tmp)
-      set(target_link_libraries_local ${target_link_libraries_local} "${${tmp}_LIBRARIES}")
+      set(target_link_libraries_local ${target_link_libraries_local} "${${tmp}_LIBRARIES}" CACHE INTERNAL "" )
 
     endforeach(sub_dependency)
 
--- external/Dependencies_BEAGLE_Shippable.cmake
+++ external/Dependencies_BEAGLE_Shippable.cmake
@@ -2,7 +2,6 @@ set (Dependencies_BEAGLE
   multivar_splines%multivariate_splines.mod
   bc03_utilities
   flap%flap.mod
-  astrofortran%lib_checking.mod
   mcfor%prosit.mod
 )
 
@@ -10,7 +9,6 @@ set (Dependencies_names_BEAGLE
   multivar_splines
   bc03_utilities
   flap
-  astrofortran
   mcfor
 )
 

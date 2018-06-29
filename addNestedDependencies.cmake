63d74930a8cabc6bed71f77f06e1717c375e771d
beagle-machine <jacopo.chevallard@esa.int>
2018-06-29 10:55:46 +0200

    Improved script to addnested dependencies

--- addNestedDependencies.cmake
+++ addNestedDependencies.cmake
@@ -14,36 +14,36 @@ function (addNestedIncludeDirectories dependency_name include_directories_list)
     set(recursion TRUE)
   endif ()
 
+  string(REPLACE "%" ";" dependency ${dependency_name})
+  list(GET dependency 0 dependency_lib_name)
+
+  #MESSAGE("dependency_lib_name: ${dependency_lib_name} ${recursion}")
+
   if (NOT recursion)
     set(include_directories_local "${include_directories_list}" CACHE INTERNAL "" )
   endif (NOT recursion)
 
-  string(REPLACE "%" ";" dependency ${dependency_name})
-  list(GET dependency 0 dependency_lib_name)
+  string(TOUPPER ${dependency_lib_name} tmp)
+  getDependencyPath(${dependency_name})
+  #MESSAGE("------------> ${${tmp}_INCLUDE_DIR}")
+  set(include_directories_local ${include_directories_local} "${${tmp}_INCLUDE_DIR}" CACHE INTERNAL "" )
+
+  #MESSAGE("include_directories_local: ${include_directories_local}")
 
   include(Dependencies_${dependency_lib_name}${SHIPPABLE_SUFFIX} OPTIONAL RESULT_VARIABLE NESTED_DEPENDENCIES)
 
   if (NESTED_DEPENDENCIES)
 
-    set(include_directories_local "${include_directories_list}")
-
     foreach (sub_dependency ${Dependencies_${dependency_lib_name}})
-
-      string(REPLACE "%" ";" sub_dependency_name ${sub_dependency})
-      list(GET sub_dependency_name 0 sub_dependency_lib_name)
 
       # Library name
       addNestedIncludeDirectories(${sub_dependency} "${include_directories_local}" TRUE)
-      getDependencyPath(${sub_dependency})
-
-      string(TOUPPER ${sub_dependency_lib_name} tmp)
-      set(include_directories_local ${include_directories_local} "${${tmp}_INCLUDE_DIR}" CACHE INTERNAL "")
 
     endforeach(sub_dependency)
 
-    set(include_directories_list ${include_directories_local} PARENT_SCOPE)
-
   ENDif (NESTED_DEPENDENCIES)
+
+  set(include_directories_list ${include_directories_local} PARENT_SCOPE)
 
 endfunction()
 
@@ -56,18 +56,28 @@ function (addNestedLibraries dependency_name target_link_libraries_list)
   set(recursion FALSE)
   set (extra_macro_args ${ARGN})
 
+  #MESSAGE("")
   # Did we get any optional args?
   list(LENGTH extra_macro_args num_extra_args)
   if (${num_extra_args} GREATER 0)
     set(recursion TRUE)
   endif ()
 
+  string(REPLACE "%" ";" dependency ${dependency_name})
+  list(GET dependency 0 dependency_lib_name)
+
+  #MESSAGE("dependency_lib_name: ${dependency_lib_name} ${recursion}")
+
   if (NOT recursion)
     set(target_link_libraries_local "${target_link_libraries_list}" CACHE INTERNAL "" )
   endif (NOT recursion)
 
-  string(REPLACE "%" ";" dependency ${dependency_name})
-  list(GET dependency 0 dependency_lib_name)
+
+  string(TOUPPER ${dependency_lib_name} tmp)
+  getDependencyPath(${dependency_lib_name})
+  set(target_link_libraries_local ${target_link_libraries_local} "${${tmp}_LIBRARIES}" CACHE INTERNAL "" )
+
+  #MESSAGE("target_link_libraries_local: ${target_link_libraries_local}")
 
   include(Dependencies_${dependency_lib_name}${SHIPPABLE_SUFFIX} OPTIONAL RESULT_VARIABLE NESTED_DEPENDENCIES)
 
@@ -75,20 +85,17 @@ function (addNestedLibraries dependency_name target_link_libraries_list)
 
     foreach (sub_dependency ${Dependencies_${dependency_lib_name}})
 
-      string(REPLACE "%" ";" sub_dependency_name ${sub_dependency})
-      list(GET sub_dependency_name 0 sub_dependency_lib_name)
-
       # Library name
       addNestedLibraries(${sub_dependency} "${target_link_libraries_local}" TRUE)
-      getDependencyPath(${sub_dependency})
 
-      string(TOUPPER ${sub_dependency_lib_name} tmp)
-      set(target_link_libraries_local ${target_link_libraries_local} "${${tmp}_LIBRARIES}" CACHE INTERNAL "" )
+      #string(TOUPPER ${sub_dependency_lib_name} tmp)
+      #set(target_link_libraries_local ${target_link_libraries_local} "${${tmp}_LIBRARIES}" CACHE INTERNAL "" )
 
     endforeach(sub_dependency)
 
-    set(target_link_libraries_list ${target_link_libraries_local} PARENT_SCOPE)
+  endif (NESTED_DEPENDENCIES)
 
-  ENDif (NESTED_DEPENDENCIES)
+
+  set(target_link_libraries_list ${target_link_libraries_local} PARENT_SCOPE)
 
 endfunction()

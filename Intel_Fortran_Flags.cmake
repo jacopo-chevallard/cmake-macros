135479705d474300e4fe458d06fdf4cb8fed8d66
Jacopo Chevallard <jacopo.chevallard@gmail.com>
2019-02-04 16:37:16 +0100

    Trying to solve compilation errors in jacopo-chevallard/BEAGLE#231, by inverting the order of the flags we can easily add the -fPIC flag with CMake

--- Intel_Fortran_Flags.cmake
+++ Intel_Fortran_Flags.cmake
@@ -1,8 +1,8 @@
 MESSAGE ( STATUS "Adding default compiler flags for Intel compiler")
 #set ( CMAKE_VERBOSE_MAKEFILE ON )
-#set ( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -DLAPACK -g -O0 -cpp -heap-arrays -check all -debug all -ftrapuv  -gen-interfaces -warn interfaces -warn unused -traceback" )
-set ( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -DLAPACK -g -O2 -cpp -xHost -heap-arrays" )
-set ( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -O3" )
+#set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O0 -cpp -heap-arrays -check all -debug all -ftrapuv  -gen-interfaces -warn interfaces -warn unused -traceback ${CMAKE_Fortran_FLAGS}" )
+set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O2 -cpp -xHost -heap-arrays ${CMAKE_Fortran_FLAGS}" )
+set ( CMAKE_C_FLAGS "-g -O3 ${CMAKE_C_FLAGS}" )
 
 # *************** FOR GINA: START ****************
 #set ( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -O3 -xHost -heap-arrays" )

0b5ac20da40a683f9bf1f6711b6619141ff2df42
Jacopo Chevallard <jacopo.chevallard@gmail.com>
2019-02-05 15:31:35 +0100

    Adding flag to generates position-independent code, deals with jacopo-chevallard/BEAGLE#231

--- GNU_Fortran_Flags.cmake
+++ GNU_Fortran_Flags.cmake
@@ -1,6 +1,6 @@
 MESSAGE ( STATUS "Adding default compiler flags for GNU compiler")
-#set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O0 -cpp -fcheck=all --warn-all -fbacktrace -fbounds-check -ffree-line-length-none ${CMAKE_Fortran_FLAGS}" )
-set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O3 -cpp -ffree-line-length-none -fbacktrace -fno-pie ${CMAKE_Fortran_FLAGS}" )
+#set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O0 -cpp -fcheck=all --warn-all -fbacktrace -fbounds-check -ffree-line-length-none -fPIC ${CMAKE_Fortran_FLAGS}" )
+set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O3 -cpp -ffree-line-length-none -fbacktrace -fno-pie -fPIC ${CMAKE_Fortran_FLAGS}" )
 set ( CMAKE_C_FLAGS "-g -O3 ${CMAKE_C_FLAGS}" )
 
 set ( Fortran_FLAGS_ARE_SET ON )
--- Intel_Fortran_Flags.cmake
+++ Intel_Fortran_Flags.cmake
@@ -1,7 +1,7 @@
 MESSAGE ( STATUS "Adding default compiler flags for Intel compiler")
 #set ( CMAKE_VERBOSE_MAKEFILE ON )
-#set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O0 -cpp -heap-arrays -check all -debug all -ftrapuv  -gen-interfaces -warn interfaces -warn unused -traceback ${CMAKE_Fortran_FLAGS}" )
-set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O2 -cpp -xHost -heap-arrays ${CMAKE_Fortran_FLAGS}" )
+#set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O0 -cpp -heap-arrays -check all -debug all -ftrapuv  -gen-interfaces -warn interfaces -warn unused -traceback -fpic ${CMAKE_Fortran_FLAGS}" )
+set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O2 -cpp -xHost -heap-arrays -fpic ${CMAKE_Fortran_FLAGS}" )
 set ( CMAKE_C_FLAGS "-g -O3 ${CMAKE_C_FLAGS}" )
 
 # *************** FOR GINA: START ****************

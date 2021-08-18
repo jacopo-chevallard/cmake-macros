baeeebce3f40866038cbcae51de7769dce699263
Jacopo Chevallard <jacopo.chevallard@gmail.com>
2021-08-18 12:10:50 +0200

    Removing flag which is creating issues when compiling with CMake on Github Actions

--- GNU_Fortran_Flags.cmake
+++ GNU_Fortran_Flags.cmake
@@ -1,6 +1,6 @@
 MESSAGE ( STATUS "Adding default compiler flags for GNU compiler")
-#set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O0 -cpp -fcheck=all -fbacktrace -fbounds-check -ffree-line-length-none -fPIC -fallow-invalid-boz ${CMAKE_Fortran_FLAGS}" )
-set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O3 -cpp -ffree-line-length-none -fcheck=all -fbacktrace -fbounds-check -fbacktrace -fallow-invalid-boz -fPIC ${CMAKE_Fortran_FLAGS}" )
-set ( CMAKE_C_FLAGS "-g -O3 ${CMAKE_C_FLAGS}" )
+#set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O0 -cpp -fcheck=all -fbacktrace -fbounds-check -ffree-line-length-none -fPIC -fallow-invalid-boz -Wimplicit-function-declaration ${CMAKE_Fortran_FLAGS}" )
+set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O3 -cpp -ffree-line-length-none -fcheck=all -fbacktrace -fbounds-check -fbacktrace  -Wimplicit-function-declaration -fPIC ${CMAKE_Fortran_FLAGS}" )
+set ( CMAKE_C_FLAGS "-g -O3 -w ${CMAKE_C_FLAGS}" )
 
 set ( Fortran_FLAGS_ARE_SET ON )

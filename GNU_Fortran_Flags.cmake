cfe88cc03284f4fa5c5ba738f23bb5bbab504e07
Jacopo Chevallard <jacopo.chevallard@mailfence.com>
2022-12-22 17:25:26 +0100

    Updating Fortran flags to prevent erors of the type 'Error: Type mismatch between actual argument at (1) and actual argument at (2)'

--- GNU_Fortran_Flags.cmake
+++ GNU_Fortran_Flags.cmake
@@ -1,6 +1,6 @@
 MESSAGE ( STATUS "Adding default compiler flags for GNU compiler")
-#set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O0 -cpp -fcheck=all -fbacktrace -fbounds-check -ffree-line-length-none -fPIC -fallow-invalid-boz -Wimplicit-function-declaration ${CMAKE_Fortran_FLAGS}" )
-set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O3 -cpp -ffree-line-length-none -fbacktrace -Wimplicit-function-declaration -fPIC ${CMAKE_Fortran_FLAGS}" )
+#set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O0 -cpp -fcheck=all -fbacktrace -fbounds-check -ffree-line-length-none -fPIC -fallow-invalid-boz -fallow-argument-mismatch -Wimplicit-function-declaration ${CMAKE_Fortran_FLAGS}" )
+set ( CMAKE_Fortran_FLAGS "-DLAPACK -g -O2 -cpp  -fbounds-check -ffree-line-length-none -fbacktrace -fallow-argument-mismatch -fallow-invalid-boz -Wimplicit-function-declaration -fPIC ${CMAKE_Fortran_FLAGS}" )
 set ( CMAKE_C_FLAGS "-g -O3 -w ${CMAKE_C_FLAGS}" )
 
 set ( Fortran_FLAGS_ARE_SET ON )

3a1e8e4d9e885fba3f7470787fe191b2cc92e8b2
Jacopo Chevallard <jacopo.chevallard@mailfence.com>
2023-04-07 17:22:21 +0200

    Removing redundant dependencies; passing to CMake C and C++ compiler flags

--- external/Dependencies_BEAGLE.cmake
+++ external/Dependencies_BEAGLE.cmake
@@ -2,9 +2,7 @@ set (Dependencies_BEAGLE
   multivar_splines%multivariate_splines.mod
   bc03_utilities
   flap%flap.mod
-  astrofortran%lib_checking.mod
   mcfor%prosit.mod
-  fruit
 )
 
 set (Dependencies_names_BEAGLE
@@ -12,7 +10,5 @@ set (Dependencies_names_BEAGLE
   bc03_utilities
   flap
   mcfor
-  astrofortran
-  fruit
 )
 
